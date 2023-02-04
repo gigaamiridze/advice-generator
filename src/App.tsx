589d147f922c6a4cecb06419c13d6a5e5825fe12
justtfelix <justttfelix@gmail.com>
2023-02-04 23:05:07 +0400

    Added themes and its provider for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './theme/defaultTheme';
 import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,19 @@
+interface Theme {
+  colors: {
+    lightCyan: string;
+    neonGreen: string;
+    grayishBlue: string;
+    darkGrayishBlue: string;
+    darkBlue: string;
+  }
+}
+
+export const defaultTheme: Theme = {
+  colors: {
+    lightCyan: "#CEE3E9",
+    neonGreen: "#52FFA8",
+    grayishBlue: "#4E5D73",
+    darkGrayishBlue: "#323A49",
+    darkBlue: "#1F2632",
+  }
+}
