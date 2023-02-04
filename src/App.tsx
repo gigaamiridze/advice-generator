067fbec148d1b43adb98e6e12b4f849f9c23b1fd
justtfelix <justttfelix@gmail.com>
2023-02-04 22:35:11 +0400

    Added global styles using styled-components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   )
 }
 
