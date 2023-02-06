cabe2fee0be8d6f69578e087fc9094d7ce184858
justtfelix <justttfelix@gmail.com>
2023-02-06 15:29:21 +0400

    Added dice button hover effect

--- src/components/Dice.ts
+++ src/components/Dice.ts
@@ -5,6 +5,11 @@ const Dice = styled.img`
   height: 60px;
   border-radius: 50%;
   cursor: pointer;
+  transition: box-shadow .25s ease-in-out;
+
+  &:hover {
+    box-shadow: 0 0 25px ${({theme}) => theme.colors.neonGreen};
+  }
 `;
 
 export default Dice;
